9e672896208b81df4257669611e8cbcc48134b01
Diamondra <diamondrarazandrinapela@gmail.com>
2023-11-10 03:40:30 +0100

    ajout test.sh

---
+++ Tp_Github/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "clone repo"
+git clone Url repo
+
+cd nom repo
+
+ls -al
+
+echo "ajouter un fichier html"
+touch index.html
+
+echo "ajouter un dossier"
+mkdir new folder
+
+echo "add"
+git add argument
+
+echo "commit"
+git commit -m "message"
+
+echo "push"
+git push origin name
+
+echo "endex.html" > "<h1> Hello World! </h1>"
+
+Eof
+This is a multi-line string.
+!Eof
+
